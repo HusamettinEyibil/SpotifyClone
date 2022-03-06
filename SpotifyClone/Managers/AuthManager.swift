49b6d11d41da23a6121e40c362f36c9546632279
Hüsamettin Eyibil <husamettin.eyibil@huawei.com>
2022-03-06 16:40:37 +0300

    Create Welcome and Auth screens, AuthManager; construct authentication structure.

--- SpotifyClone/Managers/AuthManager.swift
+++ SpotifyClone/Managers/AuthManager.swift
@@ -10,7 +10,21 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "a0aa03faf32c4d8a84b1eada0bea5ff3"
+        static let clientSecret = "c06c5409af2244dd9571e231c3ad58af"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +44,17 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(code: String, completion: @escaping ((Bool) -> Void)) {
+        // Get Token
+    }
+    
+    public func refreshAccessToken() {
+        
+    }
+    
+    public func cacheToken() {
+        
     }
 }
